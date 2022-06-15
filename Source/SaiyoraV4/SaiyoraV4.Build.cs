f0028acfab3b9648af2a37a6bcb5370b39bc13d0
Saiyoran <alecsimpson12345@gmail.com>
2022-06-15 18:22:29 -0400

    Update to 5.0
    Updated to UE5, added NetCore to Build.cs to support FastArrays.

--- Source/SaiyoraV4/SaiyoraV4.Build.cs
+++ Source/SaiyoraV4/SaiyoraV4.Build.cs
@@ -8,7 +8,7 @@ public class SaiyoraV4 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTags" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTags", "NetCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
