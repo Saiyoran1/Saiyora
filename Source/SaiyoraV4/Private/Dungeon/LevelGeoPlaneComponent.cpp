58d8037f7e5adec7c3315efcacc80a38da61eaf5
Saiyoran1 <alecsimpson12345@gmail.com>
2022-09-16 02:44:56 -0400

    Plane Swap Materials
    - Started on a new approach (instead of using post-process shading) to swap object materials when a local player plane swaps.
    - This approach uses a new component (not the default Plane component which is meant to handle combat plane swapping and restrictions) created strictly for level geometry. Combat actors will use their post-process purple shading still. The new component simply collects references to all meshes and materials, saves them off, then sets them to a defined default xplane material when the local player is xplane from them, and sets them back when the local player is in the same plane.
    - The next step is handling collision. Objects need to collide with players when in the same plane, but not collide when cross plane except for some kind of overlap event to a component that allows or restricts plane swapping when inside. The most straightforward but annoying solution might be to simply duplicate WorldDynamic, WorldStatic, Visiblity, Pawn, etc. to have a second set of collision (xplane collision).

---
+++ Source/SaiyoraV4/Private/Dungeon/LevelGeoPlaneComponent.cpp
@@ -0,0 +1,70 @@
+﻿#include "LevelGeoPlaneComponent.h"
+#include "PlaneComponent.h"
+#include "SaiyoraCombatInterface.h"
+#include "Kismet/GameplayStatics.h"
+
+ULevelGeoPlaneComponent::ULevelGeoPlaneComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void ULevelGeoPlaneComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	const APlayerController* LocalPC = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	if (IsValid(LocalPC))
+	{
+		const APawn* LocalPawn = LocalPC->GetPawn();
+		if (IsValid(LocalPawn))
+		{
+			LocalPlayerPlaneComponent = ISaiyoraCombatInterface::Execute_GetPlaneComponent(LocalPawn);
+			if (IsValid(LocalPlayerPlaneComponent))
+			{
+				LocalPlayerPlaneComponent->OnPlaneSwapped.AddDynamic(this, &ULevelGeoPlaneComponent::OnLocalPlayerPlaneSwap);
+				bIsRenderedXPlane = UPlaneComponent::CheckForXPlane(LocalPlayerPlaneComponent->GetCurrentPlane(), DefaultPlane);
+				TArray<UMeshComponent*> OwnerMeshes;
+				GetOwner()->GetComponents<UMeshComponent>(OwnerMeshes);
+				for (UMeshComponent* Mesh : OwnerMeshes)
+				{
+					FMeshMaterials MeshMaterials;
+					TArray<FName> Names = Mesh->GetMaterialSlotNames();
+					for (const FName Name : Names)
+					{
+						int32 Index = Mesh->GetMaterialIndex(Name);
+						MeshMaterials.Materials.Add(Index, Mesh->GetMaterial(Index));
+						if (bIsRenderedXPlane)
+						{
+							Mesh->SetMaterial(Index, XPlaneMaterial);
+						}
+					}
+					Materials.Add(Mesh, MeshMaterials);
+				}
+			}
+		}
+	}
+	
+}
+
+void ULevelGeoPlaneComponent::OnLocalPlayerPlaneSwap(const ESaiyoraPlane PreviousPlane, const ESaiyoraPlane NewPlane,
+	UObject* Source)
+{
+	const bool bPreviouslyXPlane = bIsRenderedXPlane;
+	bIsRenderedXPlane = UPlaneComponent::CheckForXPlane(NewPlane, DefaultPlane);
+	if (bIsRenderedXPlane == bPreviouslyXPlane)
+	{
+		return;
+	}
+	TArray<UMeshComponent*> Meshes;
+	Materials.GetKeys(Meshes);
+	for (UMeshComponent* Mesh : Meshes)
+	{
+		FMeshMaterials* MeshMaterials = Materials.Find(Mesh);
+		if (MeshMaterials)
+		{
+			for (const TTuple<int32, UMaterialInterface*>& Material : MeshMaterials->Materials)
+			{
+				Mesh->SetMaterial(Material.Key, bIsRenderedXPlane ? XPlaneMaterial : Material.Value);
+			}
+		}
+	}
+}
