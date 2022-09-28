409ef2d2ee30107b7813d14b6f7acaaba70d1509
Saiyoran1 <alecsimpson12345@gmail.com>
2022-09-28 15:31:56 -0400

    Reload animations
    - Added functionality to auto play a reload montage on every non-initial tick of a reload cast (or on the initial tick of an instant reload cast).
    - Montage speed changes based on reload cast length.

--- Source/SaiyoraV4/Private/CombatSystem/Abilities/Weapons/Reload.cpp
+++ Source/SaiyoraV4/Private/CombatSystem/Abilities/Weapons/Reload.cpp
@@ -2,6 +2,7 @@
 #include "AbilityComponent.h"
 #include "AmmoResource.h"
 #include "ResourceHandler.h"
+#include "SaiyoraPlayerCharacter.h"
 
 UReload::UReload()
 {
@@ -18,6 +19,7 @@ UReload::UReload()
 void UReload::PreInitializeAbility_Implementation()
 {
 	Super::PreInitializeAbility_Implementation();
+	OwningPlayer = Cast<ASaiyoraPlayerCharacter>(GetHandler()->GetOwner());
 	if (GetHandler()->GetOwnerRole() != ROLE_SimulatedProxy && IsValid(GetHandler()->GetResourceHandlerRef()))
 	{
 		UResource* Ammo = GetHandler()->GetResourceHandlerRef()->FindActiveResource(UAmmoResource::StaticClass());
@@ -35,6 +37,10 @@ void UReload::PreInitializeAbility_Implementation()
 void UReload::OnPredictedTick_Implementation(const int32 TickNumber)
 {
 	Super::OnPredictedTick_Implementation(TickNumber);
+	if (CastType == EAbilityCastType::Instant || TickNumber != GetNumberOfTicks())
+	{
+		StartReloadMontage();
+	}
 	if (GetHandler()->GetOwnerRole() != ROLE_Authority && CastType == EAbilityCastType::Instant || TickNumber == NonInitialTicks)
 	{
 		ActivateCastRestriction(FSaiyoraCombatTags::Get().AbilityDoubleReloadRestriction);
@@ -42,13 +48,67 @@ void UReload::OnPredictedTick_Implementation(const int32 TickNumber)
 	}
 }
 
+void UReload::OnServerTick_Implementation(const int32 TickNumber)
+{
+	Super::OnServerTick_Implementation(TickNumber);
+	if (IsValid(OwningPlayer) && !OwningPlayer->IsLocallyControlled() && CastType == EAbilityCastType::Instant || TickNumber != GetNumberOfTicks())
+	{
+		StartReloadMontage();
+	}
+}
+
+void UReload::OnSimulatedTick_Implementation(const int32 TickNumber)
+{
+	Super::OnSimulatedTick_Implementation(TickNumber);
+	if (CastType == EAbilityCastType::Instant || TickNumber != GetNumberOfTicks())
+	{
+		StartReloadMontage();
+	}
+}
+
 void UReload::OnMisprediction_Implementation(const int32 PredictionID, const ECastFailReason FailReason)
 {
 	Super::OnMisprediction_Implementation(PredictionID, FailReason);
+	CancelReloadMontage();
 	if (bWaitingOnReloadResult)
 	{
 		bWaitingOnReloadResult = false;
 		DeactivateCastRestriction(FSaiyoraCombatTags::Get().AbilityDoubleReloadRestriction);
+	}
+}
+
+void UReload::OnPredictedCancel_Implementation()
+{
+	Super::OnPredictedCancel_Implementation();
+	CancelReloadMontage();
+}
+
+void UReload::OnServerCancel_Implementation()
+{
+	Super::OnServerCancel_Implementation();
+	CancelReloadMontage();
+}
+
+void UReload::OnSimulatedCancel_Implementation()
+{
+	Super::OnSimulatedCancel_Implementation();
+	CancelReloadMontage();
+}
+
+void UReload::StartReloadMontage()
+{
+	if (IsValid(ReloadMontage) && IsValid(OwningPlayer))
+	{
+		const float PlayRate = CastType == EAbilityCastType::Instant ? 1.0f : FMath::Max(ReloadMontage->GetPlayLength() / (GetHandler()->GetCurrentCastLength() / GetNumberOfTicks()), 0.01f);
+		OwningPlayer->GetMesh()->GetAnimInstance()->Montage_Play(ReloadMontage, PlayRate);
+	}
+}
+
+void UReload::CancelReloadMontage()
+{
+	if (IsValid(OwningPlayer) && OwningPlayer->GetMesh()->GetAnimInstance()->Montage_IsPlaying(ReloadMontage))
+	{
+		OwningPlayer->GetMesh()->GetAnimInstance()->Montage_Stop(0.0f, ReloadMontage);
 	}
 }
 
--- Source/SaiyoraV4/Public/CombatSystem/Abilities/Weapons/Reload.h
+++ Source/SaiyoraV4/Public/CombatSystem/Abilities/Weapons/Reload.h
@@ -3,6 +3,8 @@
 #include "CombatAbility.h"
 #include "Reload.generated.h"
 
+class ASaiyoraPlayerCharacter;
+
 UCLASS(Blueprintable)
 class SAIYORAV4_API UReload : public UCombatAbility
 {
@@ -14,12 +16,22 @@ public:
 
 	virtual void PreInitializeAbility_Implementation() override;
 	virtual void OnPredictedTick_Implementation(const int32 TickNumber) override;
+	virtual void OnServerTick_Implementation(const int32 TickNumber) override;
+	virtual void OnSimulatedTick_Implementation(const int32 TickNumber) override;
 	virtual void OnMisprediction_Implementation(const int32 PredictionID, const ECastFailReason FailReason) override;
+	virtual void OnPredictedCancel_Implementation() override;
+	virtual void OnServerCancel_Implementation() override;
+	virtual void OnSimulatedCancel_Implementation() override;
 
 private:
 
+    UPROPERTY(EditDefaultsOnly, Category = "Reload")
+    UAnimMontage* ReloadMontage;
+
 	UPROPERTY()
 	UResource* AmmoRef;
+	UPROPERTY()
+	ASaiyoraPlayerCharacter* OwningPlayer;
 	UFUNCTION()
 	void OnResourceAdded(UResource* NewResource);
 	void SetupAmmoTracking(UResource* AmmoResource);
@@ -27,4 +39,6 @@ private:
 	void OnAmmoChanged(UResource* Resource, UObject* ChangeSource, const FResourceState& PreviousState, const FResourceState& NewState);
 	bool bWaitingOnReloadResult = false;
 	bool bFullAmmo = false;
+	void StartReloadMontage();
+	void CancelReloadMontage();
 };
