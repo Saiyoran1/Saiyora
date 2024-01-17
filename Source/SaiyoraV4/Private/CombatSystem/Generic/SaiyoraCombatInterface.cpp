82b9e023ebfe8305d8aee5bf49bc19d0b3779a9f
Saiyoran1 <alecsimpson12345@gmail.com>
2024-01-17 02:36:35 -0500

    Combat Interface Getter Defaults
    - Added default implementations to all of the component getters in SaiyoraCombatInterface. These cache the value after calling FindComponentByClass, so if I forget to implement them in a Blueprint things will still work, I'll just get a warning to the log about needing to implement them.

--- Source/SaiyoraV4/Private/CombatSystem/Generic/SaiyoraCombatInterface.cpp
+++ Source/SaiyoraV4/Private/CombatSystem/Generic/SaiyoraCombatInterface.cpp
@@ -1,1 +1,154 @@
 #include "SaiyoraCombatInterface.h"
+#include "AbilityComponent.h"
+#include "BuffHandler.h"
+#include "CombatStatusComponent.h"
+#include "CrowdControlHandler.h"
+#include "DamageHandler.h"
+#include "ResourceHandler.h"
+#include "SaiyoraMovementComponent.h"
+#include "StatHandler.h"
+#include "ThreatHandler.h"
+
+UCombatStatusComponent* ISaiyoraCombatInterface::GetCombatStatusComponent_Implementation() const
+{
+	UE_LOG(LogTemp, Warning, TEXT("Called default implementation of GetCombatStatusComponent for object %s. Please override this to return a pointer."), *_getUObject()->GetName());
+	if (bCachedCombatStatusComponent)
+	{
+		return CachedCombatStatusComponent;
+	}
+	bCachedCombatStatusComponent = true;
+	const AActor* ImplementingActor = Cast<AActor>(_getUObject());
+	if (IsValid(ImplementingActor))
+	{
+		CachedCombatStatusComponent = ImplementingActor->FindComponentByClass<UCombatStatusComponent>();
+	}
+	return CachedCombatStatusComponent;
+}
+
+UDamageHandler* ISaiyoraCombatInterface::GetDamageHandler_Implementation() const
+{
+	UE_LOG(LogTemp, Warning, TEXT("Called default implementation of GetDamageHandler for object %s. Please override this to return a pointer."), *_getUObject()->GetName());
+	if (bCachedDamageHandler)
+	{
+		return CachedDamageHandler;
+	}
+	bCachedDamageHandler = true;
+	const AActor* ImplementingActor = Cast<AActor>(_getUObject());
+	if (IsValid(ImplementingActor))
+	{
+		CachedDamageHandler = ImplementingActor->FindComponentByClass<UDamageHandler>();
+	}
+	return CachedDamageHandler;
+}
+
+UBuffHandler* ISaiyoraCombatInterface::GetBuffHandler_Implementation() const
+{
+	UE_LOG(LogTemp, Warning, TEXT("Called default implementation of GetBuffHandler for object %s. Please override this to return a pointer."), *_getUObject()->GetName());
+	if (bCachedBuffHandler)
+	{
+		return CachedBuffHandler;
+	}
+	bCachedBuffHandler = true;
+	const AActor* ImplementingActor = Cast<AActor>(_getUObject());
+	if (IsValid(ImplementingActor))
+	{
+		CachedBuffHandler = ImplementingActor->FindComponentByClass<UBuffHandler>();
+	}
+	return CachedBuffHandler;
+}
+
+UAbilityComponent* ISaiyoraCombatInterface::GetAbilityComponent_Implementation() const
+{
+	UE_LOG(LogTemp, Warning, TEXT("Called default implementation of GetAbilityComponent for object %s. Please override this to return a pointer."), *_getUObject()->GetName());
+	if (bCachedAbilityComponent)
+	{
+		return CachedAbilityComponent;
+	}
+	bCachedAbilityComponent = true;
+	const AActor* ImplementingActor = Cast<AActor>(_getUObject());
+	if (IsValid(ImplementingActor))
+	{
+		CachedAbilityComponent = ImplementingActor->FindComponentByClass<UAbilityComponent>();
+	}
+	return CachedAbilityComponent;
+}
+
+UCrowdControlHandler* ISaiyoraCombatInterface::GetCrowdControlHandler_Implementation() const
+{
+	UE_LOG(LogTemp, Warning, TEXT("Called default implementation of GetCrowdControlHandler for object %s. Please override this to return a pointer."), *_getUObject()->GetName());
+	if (bCachedCrowdControlHandler)
+	{
+		return CachedCrowdControlHandler;
+	}
+	bCachedCrowdControlHandler = true;
+	const AActor* ImplementingActor = Cast<AActor>(_getUObject());
+	if (IsValid(ImplementingActor))
+	{
+		CachedCrowdControlHandler = ImplementingActor->FindComponentByClass<UCrowdControlHandler>();
+	}
+	return CachedCrowdControlHandler;
+}
+
+UResourceHandler* ISaiyoraCombatInterface::GetResourceHandler_Implementation() const
+{
+	UE_LOG(LogTemp, Warning, TEXT("Called default implementation of GetResourceHandler for object %s. Please override this to return a pointer."), *_getUObject()->GetName());
+	if (bCachedResourceHandler)
+	{
+		return CachedResourceHandler;
+	}
+	bCachedResourceHandler = true;
+	const AActor* ImplementingActor = Cast<AActor>(_getUObject());
+	if (IsValid(ImplementingActor))
+	{
+		CachedResourceHandler = ImplementingActor->FindComponentByClass<UResourceHandler>();
+	}
+	return CachedResourceHandler;
+}
+
+UStatHandler* ISaiyoraCombatInterface::GetStatHandler_Implementation() const
+{
+	UE_LOG(LogTemp, Warning, TEXT("Called default implementation of GetStatHandler for object %s. Please override this to return a pointer."), *_getUObject()->GetName());
+	if (bCachedStatHandler)
+	{
+		return CachedStatHandler;
+	}
+	bCachedStatHandler = true;
+	const AActor* ImplementingActor = Cast<AActor>(_getUObject());
+	if (IsValid(ImplementingActor))
+	{
+		CachedStatHandler = ImplementingActor->FindComponentByClass<UStatHandler>();
+	}
+	return CachedStatHandler;
+}
+
+UThreatHandler* ISaiyoraCombatInterface::GetThreatHandler_Implementation() const
+{
+	UE_LOG(LogTemp, Warning, TEXT("Called default implementation of GetThreatHandler for object %s. Please override this to return a pointer."), *_getUObject()->GetName());
+	if (bCachedThreatHandler)
+	{
+		return CachedThreatHandler;
+	}
+	bCachedThreatHandler = true;
+	const AActor* ImplementingActor = Cast<AActor>(_getUObject());
+	if (IsValid(ImplementingActor))
+	{
+		CachedThreatHandler = ImplementingActor->FindComponentByClass<UThreatHandler>();
+	}
+	return CachedThreatHandler;
+}
+
+USaiyoraMovementComponent* ISaiyoraCombatInterface::GetCustomMovementComponent_Implementation() const
+{
+	UE_LOG(LogTemp, Warning, TEXT("Called default implementation of GetCustomMovementComponent for object %s. Please override this to return a pointer."), *_getUObject()->GetName());
+	if (bCachedMovementComponent)
+	{
+		return CachedMovementComponent;
+	}
+	bCachedMovementComponent = true;
+	const AActor* ImplementingActor = Cast<AActor>(_getUObject());
+	if (IsValid(ImplementingActor))
+	{
+		CachedMovementComponent = ImplementingActor->FindComponentByClass<USaiyoraMovementComponent>();
+	}
+	return CachedMovementComponent;
+}
--- Source/SaiyoraV4/Public/CombatSystem/Generic/SaiyoraCombatInterface.h
+++ Source/SaiyoraV4/Public/CombatSystem/Generic/SaiyoraCombatInterface.h
@@ -23,39 +23,92 @@ class SAIYORAV4_API ISaiyoraCombatInterface
 {
 	GENERATED_BODY()
 
-//Component Getters
+#pragma region Component Getters
+
+	/*
+	 * These getters are for getting the various components involved in combat.
+	 * The default implementation calls FindComponentByClass the first time, and caches off the result, returning it on subsequent calls.
+	 * This should be overridden in derived classes to directly return the component pointer or nullptr, but I forget to do that sometimes.
+	 */
 	
 public:
-	
+
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Damage")
 	UDamageHandler* GetDamageHandler() const;
-	virtual UDamageHandler* GetDamageHandler_Implementation() const { return nullptr; }
+	virtual UDamageHandler* GetDamageHandler_Implementation() const;
+	
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Buffs")
 	UBuffHandler* GetBuffHandler() const;
-	virtual UBuffHandler* GetBuffHandler_Implementation() const { return nullptr; }
+	virtual UBuffHandler* GetBuffHandler_Implementation() const;
+	
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Abilities")
 	UAbilityComponent* GetAbilityComponent() const;
-	virtual UAbilityComponent* GetAbilityComponent_Implementation() const { return nullptr; }
+	virtual UAbilityComponent* GetAbilityComponent_Implementation() const;
+	
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Crowd Control")
 	UCrowdControlHandler* GetCrowdControlHandler() const;
-	virtual UCrowdControlHandler* GetCrowdControlHandler_Implementation() const { return nullptr; }
+	virtual UCrowdControlHandler* GetCrowdControlHandler_Implementation() const;
+	
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Resources")
 	UResourceHandler* GetResourceHandler() const;
-	virtual UResourceHandler* GetResourceHandler_Implementation() const { return nullptr; }
+	virtual UResourceHandler* GetResourceHandler_Implementation() const;
+	
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Stats")
 	UStatHandler* GetStatHandler() const;
-	virtual UStatHandler* GetStatHandler_Implementation() const { return nullptr; }
+	virtual UStatHandler* GetStatHandler_Implementation() const;
+	
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Plane")
 	UCombatStatusComponent* GetCombatStatusComponent() const;
-	virtual UCombatStatusComponent* GetCombatStatusComponent_Implementation() const { return nullptr; }
+	virtual UCombatStatusComponent* GetCombatStatusComponent_Implementation() const;
+	
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Threat")
 	UThreatHandler* GetThreatHandler() const;
-	virtual UThreatHandler* GetThreatHandler_Implementation() const { return nullptr; }
+	virtual UThreatHandler* GetThreatHandler_Implementation() const;
+	
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Movement")
 	USaiyoraMovementComponent* GetCustomMovementComponent() const;
-	virtual USaiyoraMovementComponent* GetCustomMovementComponent_Implementation() const { return nullptr; }
+	virtual USaiyoraMovementComponent* GetCustomMovementComponent_Implementation() const;
 
-//Socket Getters
+private:
+	
+	mutable bool bCachedDamageHandler = false;
+	mutable UDamageHandler* CachedDamageHandler = nullptr;
+
+	mutable bool bCachedBuffHandler = false;
+	mutable UBuffHandler* CachedBuffHandler = nullptr;
+
+	mutable bool bCachedAbilityComponent = false;
+	mutable UAbilityComponent* CachedAbilityComponent = nullptr;
+
+	mutable bool bCachedCrowdControlHandler = false;
+	mutable UCrowdControlHandler* CachedCrowdControlHandler = nullptr;
+
+	mutable bool bCachedResourceHandler = false;
+	mutable UResourceHandler* CachedResourceHandler = nullptr;
+
+	mutable bool bCachedStatHandler = false;
+	mutable UStatHandler* CachedStatHandler = nullptr;
+
+	mutable bool bCachedCombatStatusComponent = false;
+	mutable UCombatStatusComponent* CachedCombatStatusComponent = nullptr;
+
+	mutable bool bCachedThreatHandler = false;
+	mutable UThreatHandler* CachedThreatHandler = nullptr;
+
+	mutable bool bCachedMovementComponent = false;
+	mutable USaiyoraMovementComponent* CachedMovementComponent = nullptr;
+
+#pragma endregion 
+
+#pragma region Socket Getters
+
+	/*
+	 * These are getters for various component sockets required for combat.
+	 * These include sockets for where visuals (such as a health bar or floating damage text) should display  for an actor,
+	 * as well as sockets that are used to predict and validate third-person aiming, or place weapons correctly.
+	 */
+
+public:
 
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Damage")
 	USceneComponent* GetFloatingHealthSocket(FName& SocketName) const;
@@ -75,4 +128,7 @@ public:
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Weapon")
 	USceneComponent* GetSecondaryWeaponSocket(FName& SocketName) const;
 	virtual USceneComponent* GetSecondaryWeaponSocket_Implementation(FName& SocketName) const { SocketName = NAME_None; return nullptr; }
+
+#pragma endregion
+	
 };
