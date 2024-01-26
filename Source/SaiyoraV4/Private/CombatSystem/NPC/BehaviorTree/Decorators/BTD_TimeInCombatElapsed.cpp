a22f2e29896d588c4d03a645ef9db20b1b995d77
Saiyoran1 <alecsimpson12345@gmail.com>
2024-01-26 03:00:57 -0500

    Decorator Experiment
    - Trying to learn how to make an event-based decorator in c++.

---
+++ Source/SaiyoraV4/Private/CombatSystem/NPC/BehaviorTree/Decorators/BTD_TimeInCombatElapsed.cpp
@@ -0,0 +1,44 @@
+﻿#include "BehaviorTree/Decorators/BTD_TimeInCombatElapsed.h"
+
+#include "GameFramework/GameStateBase.h"
+
+UBTD_TimeInCombatElapsed::UBTD_TimeInCombatElapsed()
+{
+	NodeName = "Time in Combat Elapsed";
+	bNotifyBecomeRelevant = true;
+	bNotifyCeaseRelevant = true;
+	bCreateNodeInstance = true;
+	//There's a macro that works for these, but I don't understand it.
+}
+
+void UBTD_TimeInCombatElapsed::OnBecomeRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	UE_LOG(LogTemp, Warning, TEXT("My stupid decorator became relevant!"));
+	CombatStartTime = OwnerComp.GetWorld()->GetGameState()->GetServerWorldTimeSeconds();
+	UE_LOG(LogTemp, Warning, TEXT("Set combat start time for %s as %f."), *OwnerComp.GetOwner()->GetName(), CombatStartTime);
+	FTimerDelegate LockoutDelegate;
+	LockoutDelegate.BindUFunction(this, FName("OnLockoutComplete"), TWeakObjectPtr<UBehaviorTreeComponent>(&OwnerComp));
+	OwnerComp.GetWorld()->GetTimerManager().SetTimer(LockoutHandle, LockoutDelegate, LockoutDuration, false);
+}
+
+void UBTD_TimeInCombatElapsed::OnCeaseRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	UE_LOG(LogTemp, Warning, TEXT("My stupid decorator became not relevant!"));
+	OwnerComp.GetWorld()->GetTimerManager().ClearTimer(LockoutHandle);
+}
+
+void UBTD_TimeInCombatElapsed::OnLockoutComplete(TWeakObjectPtr<UBehaviorTreeComponent> OwnerComp)
+{
+	if (!OwnerComp.IsValid())
+	{
+		return;
+	}
+	ConditionalFlowAbort(*OwnerComp, EBTDecoratorAbortRequest::ConditionResultChanged);
+}
+
+bool UBTD_TimeInCombatElapsed::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	const float TimePassed = (OwnerComp.GetWorld()->GetGameState()->GetServerWorldTimeSeconds() - CombatStartTime);
+	return TimePassed >= LockoutDuration;
+}
+
---
+++ Source/SaiyoraV4/Public/CombatSystem/NPC/BehaviorTree/Decorators/BTD_TimeInCombatElapsed.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "BTD_TimeInCombatElapsed.generated.h"
+
+//Decorator that passes once a certain amount of time has passed since combat started.
+
+UCLASS()
+class SAIYORAV4_API UBTD_TimeInCombatElapsed : public UBTDecorator
+{
+	GENERATED_BODY()
+
+public:
+
+	UBTD_TimeInCombatElapsed();
+
+protected:
+	
+	virtual void OnBecomeRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	virtual void OnCeaseRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+private:
+
+	UPROPERTY(EditAnywhere, Category = "Time", meta = (AllowPrivateAccess = "true"))
+	float LockoutDuration = 1.0f;
+
+	float CombatStartTime = 0.0f;
+	FTimerHandle LockoutHandle;
+	UFUNCTION()
+	void OnLockoutComplete(TWeakObjectPtr<UBehaviorTreeComponent> OwnerComp);
+
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+
+};
